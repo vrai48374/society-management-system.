c07e97744c5697f6ba2323e18c13b539434a66b1
vrai48374 <vrai48374@gmail.com>
2025-09-09 17:31:06 +0530

    fix: update cookie settings to always use secure and none for sameSite

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,8 +28,8 @@ export const login = async (req, res, next) => {
     
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     
@@ -66,8 +66,8 @@ export const register = async (req, res, next) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
