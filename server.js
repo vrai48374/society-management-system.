820b00191d02849d461a48e7fc8bafbb34f25bff
vrai48374 <vrai48374@gmail.com>
2025-09-09 16:07:26 +0530

    fix: update CORS configuration to allow requests from all origins

--- server.js
+++ server.js
@@ -31,11 +31,7 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "https://cute-medovik-4edd9e.netlify.app",
+    "0.0.0.0"
   ],
   credentials: true,
 }));
